2fe8d38dc53d29224944a54cc06a28e546130691
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-22 09:48:42 +0330

    Add files via upload

---
+++ ConsoleApp99/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.Write($"{counter}");
+                ++counter;
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
